8e89cfe54efcf8ef8271bfc9f584fb96dddf399f
Josep Cristobal <josepc.mercader@gmail.com>
2021-05-04 21:12:00 +0200

    Subida cambios multer restaurados

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -4,11 +4,23 @@
 var express = require('express');
 var router = express.Router();
 const jwtAuth = require('../../lib/jwtAuth');
+const path = require('path');
 
 const Anuncio = require('../../models/Anuncio.js');
 const {body, validationResult} = require('express-validator');
 const configAnuncios = require('../../local_config').anuncios;
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination : path.join(__dirname, 'public/images'),
+    filename: (req,file,cb) =>{
+        cb(null, file.originalname);
+    }
+});
 
+const upload = multer({
+    storage,
+    dest: path.join(__dirname, 'public/images')
+}).single('foto')
 
 /* GET /apiv1/anuncios  */
 // Impelmentamos la autrización en la consulta
@@ -92,5 +104,35 @@ router.post('/', jwtAuth,
         }
 });
 
+//Grabación de nuevos anuncios con validación incluida y subida de imagen
+router.post('/upload', jwtAuth, upload,
+    [
+        body('nombre').not().isEmpty().trim().escape().withMessage('El campo Nombre no puede estar vacío'),
+        body('precio').isNumeric().withMessage('El precio debe ser numérico'),
+        body('venta').isBoolean().withMessage('Valores admitidos: true o false'),
+        body('tags').custom(tags => {   
+            //Validamos si los tags que nos pasan están dentro de los permitidos
+            const errorTags = Anuncio.allowedTagsEnumValidate(tags)
+
+            //Si alguno de los tags no se encuentra, lanzaremos un error indicando que tags no son admitidos
+            if (errorTags.length > 0){
+                throw new Error(`Tags no admitidos: ${errorTags}`);
+            } else {
+                return true}
+        })
+    ], 
+ async (req, res, next) =>{
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array()});
+        }
+        console.log(req.file)
+        const anuncioCreado = await Anuncio.newAnuncio(req.body,req.file.originalname);
+        res.status(201).json({result: anuncioCreado});
+    } catch (error) {
+        next(error)      
+        }
+});
 
 module.exports = router;
